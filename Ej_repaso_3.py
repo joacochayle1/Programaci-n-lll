acada3262d99875a9d383bc1c6bff864bcf3d4de
joacochayle1 <110994640+joacochayle1@users.noreply.github.com>
2024-03-08 08:18:15 -0300

    Delete Ej_repaso_3.py

--- Ej_repaso_3.py
+++
@@ -1,5 +0,0 @@
-ancho=int(input("Ingrese el ancho: "))
-alto=int(input("Ingrese el alto: "))
-caracter=input("Ingrese el caracter que va a usar: ")
-for n in range(1,alto+1):
-    print(f"{caracter*(ancho)}")
